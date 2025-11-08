d4a0e8c4156985293428bb568ce53490dbac6626
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-08 19:44:59 +0800

    Added Lib Folder to Client

---
+++ wwwroot/client/src/lib/helpers.js
@@ -0,0 +1,19 @@
+export const getStatusIconClass = (status) => {
+	const classes = {
+		Applied: 'bg-blue-500',
+		Interview: 'bg-yellow-500',
+		Offer: 'bg-green-500',
+		Rejected: 'bg-red-500'
+	}
+	return classes[status] || 'bg-gray-500'
+};
+
+export const getStatusClass = (status) => {
+	const classes = {
+		Applied: 'bg-blue-100 text-blue-700',
+		Interview: 'bg-yellow-100 text-yellow-700',
+		Offer: 'bg-green-100 text-green-700',
+		Rejected: 'bg-red-100 text-red-700'
+	}
+	return classes[status] || 'bg-gray-100 text-gray-700'
+};
