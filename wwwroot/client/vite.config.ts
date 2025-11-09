ff383a6bc02552cc6e28bbd59de49c0096cbdc42
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-09 22:35:46 +0800

    Integrated Root Env to Client

--- wwwroot/client/vite.config.ts
+++ wwwroot/client/vite.config.ts
@@ -1,26 +1,33 @@
-import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js',
-    }
-  },
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5217', // ← Your .NET port
-        changeOrigin: true
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+        'vue': 'vue/dist/vue.esm-bundler.js',
+      }
+    },
+    define: {
+      'process.env': loadEnv(mode, '../../'),
+    },
+    envDir: '../../',
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true
+    },
+    server: {
+      port: 5173,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:5217', // ← Your .NET port
+          changeOrigin: true
+        }
       }
     }
   }
-})
+});
