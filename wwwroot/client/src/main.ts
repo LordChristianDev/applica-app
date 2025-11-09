16fe9f573a56adbca72192d6fb0d9286742c55f9
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-09 22:35:17 +0800

    Added BASE URL

--- wwwroot/client/src/main.ts
+++ wwwroot/client/src/main.ts
@@ -5,6 +5,11 @@ import router from './router/router';
 import App from './App.vue';
 import './index.css';
 
+export const BASE_URL =
+	import.meta.env.MODE === "development"
+		? "http://localhost:4000/api"
+		: `${window.location.origin}/api`;
+
 const app = createApp(App);
 
 app.use(createPinia());
