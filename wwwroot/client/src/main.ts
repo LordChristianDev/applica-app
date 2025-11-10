0b0b1f4869d6d3b1cd5c7a7e1a243bb089dba277
LordChristianDev <lordchristian.dev@gmail.com>
2025-11-10 20:52:56 +0800

    Integrated Clerk Authentication

--- wwwroot/client/src/main.ts
+++ wwwroot/client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import { clerkPlugin } from '@clerk/vue';
 
 import router from './router/router';
 import App from './App.vue';
@@ -10,9 +11,16 @@ export const BASE_URL =
 		? "http://localhost:4000/api"
 		: `${window.location.origin}/api`;
 
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+	throw new Error('Add your Clerk Publishable Key to the .env file')
+}
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(clerkPlugin, { publishableKey: PUBLISHABLE_KEY });
 
 app.mount('#app');
